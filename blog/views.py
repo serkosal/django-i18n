e9593220b3014e556847c55e13f28a8424a733ad
Sergey Kosykh <s3rkosal@gmail.com>
2026-04-08 23:25:08 +0400

    added blog app with basic test views

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def index(req: HttpRequest):
+    return HttpResponse('index test')
